f3f95195d975ef8530a5eeeee2c746a39bf9c952
yezuozuo <1062276629@qq.com>
2016-09-30 11:02:20 +0800

    first commit

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zoco
+ * Date: 16/9/22
+ * Time: 17:46
+ */
+echo json_encode([
+    'a' => rand(0,1000),
+    'b' => rand(0,1000),
+    'c' => rand(0,1000),
+]);
